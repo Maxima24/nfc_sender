6fb5a0a3bca9eb0203247b31d8348390bf11c1b7
Steel Maxima <steelmaxima21@gmail.com>
2026-05-06 20:43:34 +0100

    feat:add logging to webhook

--- src/modules/payoza/payoza.controller.ts
+++ src/modules/payoza/payoza.controller.ts
@@ -5,11 +5,12 @@ import { CurrentUser } from 'src/common/decorators/current_user.decorator';
 import { InitilizeTopUp } from './dto/initiate-topup.dto';
 import { JwtGuard } from 'src/common/utils/jwt.utils';
 import { IHandleWebhookDto } from './dto/handle-webhook.dto';
+import { LoggerService } from 'src/logger/logger.service';
 
 @ApiTags("Payaza")
 @Controller('payoza')
 export class PayozaController {
-  constructor(private readonly payozaService: PayozaService) {}
+  constructor(private readonly payozaService: PayozaService,private logger:LoggerService) {}
 
   @ApiOperation({summary:'Simulate Payaza transaction'})
   @ApiBody({type:InitilizeTopUp})
@@ -22,7 +23,9 @@ export class PayozaController {
    @ApiOperation({summary:'webhook payaza calls'})
   @Post("webhook")
    @ApiBody({type:IHandleWebhookDto})
-  async initiateWebhook(@Body() body:any,@Headers('x-payaza-signature') signature: string){
+  async initiateWebhook(@Body() body:any,@Headers('x-payaza-signature') 
+  signature: string){
+      console.log(body)
      return await this.payozaService.handleWebhook(signature,body)
   }
   @ApiOperation({summary:"Initialize payment gateway"})
@@ -30,6 +33,6 @@ export class PayozaController {
   @UseGuards(JwtGuard)
  @ApiBody({type:InitilizeTopUp})
   async intializePayaza(@Body() body:InitilizeTopUp,@CurrentUser() user ){
-      return await this.payozaService.handlepayazaTopupInitialize(body,user)
+      return await this.payozaService.handlepayazaTopupInitialize(body,user.id)
   }
 }
